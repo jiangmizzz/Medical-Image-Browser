4cce74e7dbe2fd93a83cd69d857e8f44092c1aa4
jiangmizzz <2094197975@qq.com>
2024-07-09 23:03:40 +0800

    fix: delete server config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import watchFolderPlugin from "./vite-plugin-watch-folder";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    watchFolderPlugin("path/to/watched-folder"), // TODO:替换为你要监听的文件夹路径
-  ],
+  plugins: [react()],
 });
